f38e78be2d2799319ef9f739fcf115b9d0bbcf39
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-04-09 13:20:04 +0200

    setup program structure and js for switching content panes

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 const $ = require('jquery');
 
-$('li').click(function() {
-	console.log('clicked an li element');
+$('#mail').click(function(e) {
+	e.preventDefault();
+	contentLoadHtml('/views/mail.html');
 });
+
+$('#messages').click(function(e) {
+	e.preventDefault();
+	contentLoadHtml('/views/messages.html');
+});
+
+function contentLoadHtml(htmlPath) {
+	// fade out content div and empty it.
+	$('#content').fadeOut(500, function() {
+
+		$('#content').empty();
+
+		// now start loading html from views file via ajax
+		$.ajax({
+			url: __dirname + htmlPath,
+			success: function(html) {
+
+				// now we can put html back into #content and fade it in
+				$('#content').html(html).promise().done(function() {
+					$('#content').fadeIn(500);
+				});
+
+			}
+		});
+	});
+}
