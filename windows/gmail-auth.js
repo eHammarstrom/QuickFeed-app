bd9a5b88c516297d181e5fbe55229d18d38ae2a8
Emil Hammarstrom <emil.a.hammarstrom@gmail.com>
2016-04-09 23:12:01 +0200

    trying to catch token post, have to fix eventemitter memleak or just small array

--- windows/gmail-auth.js
+++ windows/gmail-auth.js
@@ -2,6 +2,9 @@ const googleAuth = require('../engine/api-auth/lama-risky-google.js');
 const $ = require('jquery');
 
 exports.startWindow = function(authWindow) {
+    // This is the first state where we load gmail auth page
+    // if done correctly we enter next callback function to catch token post
+    // if done incorrectly we exit out and throw the httpResponseCode *FOR NOW*
     googleAuth.authUrl(function(url) {
         authWindow.loadURL(url);
 
@@ -19,9 +22,23 @@ exports.startWindow = function(authWindow) {
                 console.error('Redirected to: ' + newURL);
                 console.error('Error could not reach auth page, http response: ' + httpResponseCode);
                 authWindow.close();
+            } else {
+                authWindow.show();
+                catchTokenPost(authWindow);
             }
         });
+    });
+}
 
-        authWindow.show();
+// This is the second stage, not for export as it is dependent on first stage
+function catchTokenPost(authWindow) {
+    authWindow.on('page-title-updated', function() {
+        console.log('Page title updated');
+        return;
+    });
+
+    authWindow.on('close', function() {
+        console.error('User closed window, could not catch token post');
+        return;
     });
 }
