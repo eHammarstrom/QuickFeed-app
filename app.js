1a0ace796cfca3bdaa8fde0ff59b707a757cf4f1
spiking <a.thuvesen@gmail.com>
2016-04-12 14:54:00 +0200

    Basic overview, fonts-library added etc

--- app.js
+++ app.js
@@ -9,27 +9,27 @@ const gmailAuth = require('./windows/gmail-auth.js');
 let mainWindow = null;
 
 app.on('ready', function() {
-	mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
-		minWidth: 600,
-		minHeight: 400
-	});
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        minWidth: 600,
+        minHeight: 400
+    });
 
-	mainWindow.setMenu(null); // we design and set menu in main.js instead
-	mainWindow.loadURL('file://' + __dirname + '/main.html');
-	mainWindow.openDevTools(); // remove line from production
+    mainWindow.setMenu(null); // we design and set menu in main.js instead
+    mainWindow.loadURL('file://' + __dirname + '/main.html');
+    // mainWindow.openDevTools(); // remove line from production
 
-	ipcMain.on('asynchronous-message', function(event, arg) {
-		if (arg === 'show-auth-gmail') {
-			gmailAuth.startAuthorization();
-		}
-	});
+    ipcMain.on('asynchronous-message', function(event, arg) {
+        if (arg === 'show-auth-gmail') {
+            gmailAuth.startAuthorization();
+        }
+    });
 
-	mainWindow.on('closed', function() {
-		// maybe store windows in array later and dereference all on mainWindow close.
-		// depends on if program will become multi-windowed
-		authWindow = null;
-		mainWindow = null;
-	});
+    mainWindow.on('closed', function() {
+        // maybe store windows in array later and dereference all on mainWindow close.
+        // depends on if program will become multi-windowed
+        authWindow = null;
+        mainWindow = null;
+    });
 });
