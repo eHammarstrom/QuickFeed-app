05e2b045a1de163a9f0f505135c0a1c9b8e7b8d9
Ovhagen <ovhagen@gmail.com>
2016-04-21 18:29:48 +0200

    Commit of new settings-tab

--- views/settings/settings.js
+++ views/settings/settings.js
@@ -1,27 +1,9 @@
 const gmail = require('../../engine/api-content/gmail.js');
 const $ = require('jquery');
 
-let gmailInstance = gmail.instance;
+let gmail = gmail.instance;
 
-function printMessages() {
-    gmailInstance.getMailMessageList(function(messages) {
-        let i = 1;
-        for (let key in messages) {
-            let date = new Date(messages[key].internalDate * 1000);
-
-            $('#mailTable>tbody').append(
-                $('<tr class="' + messages[key].id + '">' +
-                    '<td>' + messages[key].payload.headers['From'] + '</td>' +
-                    '<td>' + messages[key].snippet + '</td>' +
-                    '<td>' + date + '</td>' +
-                '</tr>').hide().delay(100 * i).fadeIn(1000)
-            );
-
-            i++;
-        }
+function printProfile() {
+    gmail.request.getProfile(function(profiles) {
     });
-}
-
-$('#mailTable>tbody').click(function() {
-    printMessages();
-});
+  }
