62a118985a87cc1ccd27aa0b6712ce03757345d5
Ovhagen <ovhagen@gmail.com>
2016-05-03 20:31:48 +0200

    settings css and js update

--- views/settings/settings.js
+++ views/settings/settings.js
@@ -7,7 +7,8 @@ function printProfile() {
     gmail.request.getProfile(function(profile) {
       console.log(profile.emailAddress);
       $('#gmailSettings').append(
-        ''
+        '<p id="active">' + profile.emailAddress + '</p>'
+          /*$('<p>' + profile.emailAddress + '</p>');*/
       );
     });
   }
