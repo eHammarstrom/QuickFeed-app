3e027389e83f3ba5dd6979298effa5ecfd4d0fea
Pontus Ovhagen <ovhagen@gmail.com>
2016-05-04 21:58:56 +0200

    Settings.js profile code added

--- views/settings/settings.js
+++ views/settings/settings.js
@@ -1,15 +1,32 @@
 const gmail = require('../../engine/api-content/gmail.js');
 const $ = require('jquery');
+const path = require('path');
 
-
+let activeAccounts = [];
 
 function printProfile() {
-    gmail.request.getProfile(function(profile) {
-      console.log(profile.emailAddress);
+  for (let i = 0; i < activeAccounts.length; i++) {
       $('#gmailSettings').append(
-        '<p id="active">' + profile.emailAddress + '</p>'
-          /*$('<p>' + profile.emailAddress + '</p>');*/
+        '<p id="active">' + activeAccounts[i] + '</p>'
       );
+    }
+  }
+
+  function getProfile() {
+    gmail.request.getProfile(function(profile) {
+      console.log('Added ' + profile.emailAddress);
+      activeAccounts.push(profile.emailAddress);
     });
   }
-  printProfile();
+
+ /** Document specific JQUERY **/
+
+  $(document).ajaxComplete(function(e, xhr, settings) {
+      if (settings.url === path.normalize(__dirname + '/settings.html')) {
+        console.log(activeAccounts.length);
+          if (activeAccounts.length == 0)
+            $.when(getProfile()).then(printProfile());
+            else
+            printProfile();
+      }
+  });
