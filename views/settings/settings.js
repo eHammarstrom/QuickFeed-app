435d0b480ac4bb0bd7844519d9fbe46e99b47a5c
Pontus Ovhagen <ovhagen@gmail.com>
2016-05-07 17:06:26 +0200

    Settings commit, updated active users

--- views/settings/settings.js
+++ views/settings/settings.js
@@ -3,19 +3,32 @@ const $ = require('jquery');
 const path = require('path');
 
 let activeAccounts = [];
+let currentAccount;
 
-function printProfile() {
+/**Loads the current profile and other active accounts
+    to dropdown menu*/
+function printProfiles() {
+  $('#activeButton').append(
+    currentAccount +
+    '<span class="caret"></span>'
+  );
+
   for (let i = 0; i < activeAccounts.length; i++) {
-      $('#gmailSettings').append(
-        '<p id="active">' + activeAccounts[i] + '</p>'
+      $('#dropElement').append(
+        '<div class="dropdown-divider"></div>' +
+        '<li id="user' + i + '"><a class="dropdown-item" href="#">' + activeAccounts[i] + '</a></li>'
       );
     }
   }
 
   function getProfile() {
     gmail.request.getProfile(function(profile) {
-      console.log('Added ' + profile.emailAddress);
-      activeAccounts.push(profile.emailAddress);
+      let address = profile.emailAddress;
+      if(!activeAccounts.includes(address)){
+        console.log('Added ' + address);
+        activeAccounts.push(address);
+        currentAccount = address;
+      }
     });
   }
 
@@ -24,9 +37,9 @@ function printProfile() {
   $(document).ajaxComplete(function(e, xhr, settings) {
       if (settings.url === path.normalize(__dirname + '/settings.html')) {
         console.log(activeAccounts.length);
-          if (activeAccounts.length == 0)
-            $.when(getProfile()).then(printProfile());
-            else
-            printProfile();
+        if(activeAccounts.length < 1){
+          getProfile();
+        }
+          printProfiles();
       }
   });
