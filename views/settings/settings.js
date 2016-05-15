ea0286e00110830c048946b5bad353151f875a49
Ovhagen <te11c.pontus.ovhagen@gmail.com>
2016-05-15 12:40:40 +0200

    Mereg done for settings with callback (#12)
    * Added a settings-tab
    
    * Commit of settings-tab
    
    * Commit of new settings-tab
    
    * Update settings.js
    
    * Added copyright footer and apps
    
    * css modified for settings
    
    * settings css and js update
    
    * New pull
    
    * Settings.js profile code added
    
    * Settings commit, updated active users
    
    * Settings fixed add account. And updated font-awesome
    
    * Settings fixing callback
    
    * Fixed merge Settings

--- views/settings/settings.js
+++ views/settings/settings.js
@@ -1,6 +1,7 @@
 const gmail = require('../../engine/api-content/gmail.js');
 const $ = require('jquery');
 const path = require('path');
+const ipcRenderer = require('electron').ipcRenderer;
 
 let activeAccounts = [];
 let currentAccount;
@@ -8,43 +9,50 @@ let currentAccount;
 /**Loads the current profile and other active accounts
     to dropdown menu*/
 function printProfiles() {
-    let account = currentAccount;
+  console.log(activeAccounts.length);
+  let account = currentAccount;
+  if(typeof account == 'undefined'){
+    account = 'No accounts found';
+  }
 
-    if (typeof account == 'undefined') {
-        account = 'No accounts found';
-    }
+  $('#activeButton').append(
+    account + '<span class="caret"></span>'
+  );
 
-    $('#activeButton').append(
-        account +
-        '<span class="caret"></span>'
-    );
-
-    for (let i = 0; i < activeAccounts.length; i++) {
-        console.log('Printing: ' + activeAccounts[i]);
-        $('#dropElement').append(
-            '<div class="dropdown-divider"></div>' +
-            '<li id="user' + i + '"><a class="dropdown-item" href="#">' + activeAccounts[i] + '</a></li>'
-        );
+  for (let i = 0; i < activeAccounts.length; i++) {
+      let account = activeAccounts[i];
+      if(i == 0){
+        account = '<b>' + activeAccounts[i] + '</b>';
+      }
+      $('#dropElement').append(
+        '<div class="dropdown-divider"></div>' +
+        '<li id="user' + i + '"><a class="dropdown-item" href="#">' + account + '</a></li>'
+      );
     }
 }
 
-function getProfile(callback) {
-    gmail.request.getProfile(function(profile) {
+  function getProfile(callback) {
+      gmail.request.getProfile(function(profile) {
         let address = profile.emailAddress;
-        if (!activeAccounts.includes(address)) {
-            console.log('Added: ' + address);
-            activeAccounts.push(address);
-            currentAccount = address;
-
-            callback();
+        if(!activeAccounts.includes(address)){
+          console.log('Added ' + address);
+          activeAccounts.unshift(address);
+          currentAccount = address;
         }
+        callback();
+      });
+  }
+
+  function loadAuth(){
+    $('#authLink').click(function(){
+      ipcRenderer.send('asynchronous-message','show-auth-gmail');
     });
-}
+  }
 
 /** Document specific JQUERY **/
-
-$(document).ajaxComplete(function(e, xhr, settings) {
-    if (settings.url === path.normalize(__dirname + '/settings.html'))
-        if (activeAccounts.length < 1)
-            getProfile(printProfiles);
-});
+  $(document).ajaxComplete(function(e, xhr, settings) {
+    if (settings.url === path.normalize(__dirname + '/settings.html')) {
+          loadAuth();
+          getProfile(printProfiles);
+        }
+  });
