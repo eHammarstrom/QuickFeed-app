fd3a5091e86c76e0f900b96e1e51bc2c65a44c4e
Adam Thuvesen <a.thuvesen@gmail.com>
2016-05-16 18:05:52 +0200

    Added popup view for new mail

--- views/mail/mail.js
+++ views/mail/mail.js
@@ -28,8 +28,8 @@ function printCache() {
         console.log('Cache is not empty!');
         for (let i = 0; i < gmail.cache.length; i++) {
             messageToHtml(
-                gmail.cache[i],
-                messageSpawnDelay *
+                gmail.cache[i]
+                , messageSpawnDelay *
                 ((i + 1) * messageSpawnDelayPerMessage));
         }
         return true;
@@ -39,12 +39,12 @@ function printCache() {
 }
 
 function printMessages() {
-    gmail.request.getMailMessageList(function(messages) {
+    gmail.request.getMailMessageList(function (messages) {
         let i = 1;
         for (let key in messages) {
             messageToHtml(
-                messages[key],
-                messageSpawnDelay *
+                messages[key]
+                , messageSpawnDelay *
                 (i++ * messageSpawnDelayPerMessage));
         }
         scrollPrint = true;
@@ -52,7 +52,7 @@ function printMessages() {
 }
 
 function printMessageContent(message_id) {
-    gmail.request.getMailCachedContent(message_id, function(message) {
+    gmail.request.getMailCachedContent(message_id, function (message) {
         let content = gmail.parse.getBody(message);
 
         $('.mail-display').hide();
@@ -72,7 +72,7 @@ function printMessageContent(message_id) {
 
         // get the spawned iframe, fill the contents and expand the view of it
         let $iframe = $('#' + message.id + '_iframe');
-        $iframe.ready(function() {
+        $iframe.ready(function () {
             let body = $iframe.contents().find('body');
 
             body.append(content);
@@ -96,7 +96,7 @@ function printMessageContent(message_id) {
 /** Document specific JQUERY **/
 
 // Catch the ajax call and print our email cache or grab new messages!
-$(document).ajaxComplete(function(e, xhr, settings) {
+$(document).ajaxComplete(function (e, xhr, settings) {
     //console.log(settings.url);
     //console.log(path.normalize(__dirname + '/mail.html'));
     if (settings.url === path.normalize(__dirname + '/mail.html')) {
@@ -106,7 +106,7 @@ $(document).ajaxComplete(function(e, xhr, settings) {
     }
 });
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     if (($(window).scrollTop() + $(window).height() >
             $(document).height() - $(document).height() / 3) &&
         scrollPrint === true) {
@@ -115,13 +115,28 @@ $(window).scroll(function() {
     }
 });
 
-$(document).on('click', '#sendMail', function(e) {
+$(document).on('click', '#emailModel', function (e) {
     e.preventDefault();
+    
     // Here we will toggle an overlayed 'window' and prepare for mail to be sent
+
+    var button = $(e.relatedTarget)
+    var emailModal = $(this)
+
 });
 
-$(document).on('click', '#mailTable>tbody>tr', function(e) {
+$(document).on('click', '#mailTable>tbody>tr', function (e) {
     e.preventDefault();
     //console.log(this.id);
     printMessageContent(this.id);
 });
+
+//$('#emailModel').on('show.bs.modal', function (event) {
+//  var button = $(event.relatedTarget) 
+//  var subject;
+//  var recipient = button.data('whatever') 
+//  
+//  var modal = $(this)
+//  modal.find('.modal-title').text('New Email ' + recipient)
+//  modal.find('.modal-body input').val(recipient)
+//})
